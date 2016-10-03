09092f97f72a93a97f1e34115d1f473fde4caf86
Brandon Slaght <brandonsesipod@gmail.com>
2016-10-02 23:15:46 -0400

    Sarting framework for the project.
    This uses an object oriented php library for the classes.  I included
    bootstrap 4 with flexbox, bootstrap js, and jquery.  The way this is set
    up is that you put pages in the pages directory (everything is php) and
    the index.php will automatically fill in this page from the url.  Still
    need to set up a database.  Please use bootstrap for pretty much
    everything, docs are online.  Familiarize with the code.  Also please
    tabs not spaces :100:

---
+++ includes/init.php
@@ -0,0 +1,51 @@
+<?php
+	session_start();
+
+	$GLOBALS['config'] = array(
+		'mysql' => array(
+			'host' => '',
+			'username' => '',
+			'password' => '',
+			'db' => ''
+		),
+		'remember' => array(
+			'cookie_name' => 'hash',
+			'cookie_expiry' => 604800
+		),
+		'session' => array(
+			'session_name' => 'user',
+			'token_name' => 'token'
+		)
+	);
+
+	spl_autoload_register(function($class){
+		require_once $_SERVER['DOCUMENT_ROOT'].'/classes/'.$class.'.php';
+	});
+
+	//require_once "/function/functions.php";
+	
+	$user;
+	if(cookie::exists(config::get('remember/cookie_name')) && !session::exists(config::get('session/session_name'))){
+		$hash = cookie::get(config::get('remember/cookie_name'));
+		$hashCheck = DB::getInstance()->get('user_session', array('hash', '=', $hash));
+
+		if($hashCheck->count()){
+			$user = new user($hashCheck->first()->user_id);
+			$user->login();
+		}
+	}
+
+	
+	$loggedin = false;
+
+	if(session::exists(config::get('session/session_name'))){
+		$loggedin = true;
+	}
+
+	if(isset($_GET['page'])){
+  		$page_name = $_GET['page'];
+	}
+	else{
+		$page_name = "home";
+	}
+?>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+	require_once "includes/init.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Registrar - <?php echo ucfirst($page_name); ?></title>
+		<link rel="stylesheet" type="text/css" href="/stylesheets/bootstrap.css"/>
+		<link rel="stylesheet" type="text/css" href="/stylesheets/meliora.css"/>
+		<script type="text/javascript" src="/scripts/jquery.js"></script>
+		<script type="text/javascript" src="/scripts/bootstrap.js"></script>
+		<script type="text/javascript" src="/scripts/autohidingnavbar.js"></script>
+	</head>
+	<body>
+	<!-- <?php //include("modules/header.php"); ?> -->
+	<div class="main">
+		<?php
+		if(isset($_GET['page'])){
+			include("pages/".$page_name.".php");
+		}
+		else{
+			include("pages/home.php");
+		}
+		?>
+		</div>
+	</body>
+</html>
