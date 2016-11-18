0a4e18adbe4753e77f72826149bf30570aca3aa9
cccccali <lhe4@u.rochester.edu>
2016-11-17 23:18:58 -0500

    Add files via upload

--- pages/UpdateAddress.php
+++ pages/UpdateAddress.php
@@ -6,7 +6,7 @@
               <ul>
                 <li>Fields marked with  *  must be filled in before submitting form. </li>
                 <li>If any read-only fields are incorrect, please contact your school's Registrar.</li>
-                <li>International Students: Address updates must be reported within 10 days of any change.  For more information on these requirements, please visit the <a href="#"= >International Services Office</a>.</li>
+                <li>International Students: Address updates must be reported within 10 days of any change.  For more information on these requirements, please visit the <a href="http://www.iso.rochester.edu"= >International Services Office</a>.</li>
               </ul>
 
           <form method="post" action="#" >
@@ -23,8 +23,15 @@
             <div class="form-group">
               <label for="city">City*</label>
               <input type="text" class="form-control" name="city" id="city" placeholder="Rochester">
-              <label for="street">Postal Code*</label>
+              <label for="postal code">Postal Code*</label>
               <input type="text" class="form-control" name="postal code" id="postal code" placeholder="14627">
+            </div>
+
+            <div class="form-group">
+              <label for = "country">Country</label>
+              <select name="country" class="countries" id="countryId">
+                <option value="">Select Country</option>
+              </select>
             </div>
             
             <div class="form-group">
@@ -34,12 +41,7 @@
               </select>
             </div>
 
-            <div class="form-group">
-              <label for = "country">Country</label>
-              <select name="country" class="countries" id="countryId">
-                <option value="">Select Country</option>
-              </select>
-            </div>
+
 
 
             <div class="form-group">
@@ -52,7 +54,7 @@
               <input type="tel" class="form-control" name="phone" id="tel" placeholder="(###) ###-####">
             </div>
 
-            <h2> Residential Address at the University of Rochester </h2>
+            <h2> Residential Address at the U of R </h2>
             <div class="form-group">
               <label for="street">Street1* </label>
               <input type="text" class="form-control" name="street" id="street" placeholder="500 Joseph C. Wilson Blvd" autofocus required>
@@ -63,15 +65,22 @@
             </div>
 
             <div class="form-group">
-              <label for="street">Building</label>
-              <input type="text" class="form-control" name="street" id="street" placeholder="Riverview G">
+              <label for="building">Building</label>
+              <input type="text" class="form-control" name="building" id="building" placeholder="Riverview G">
             </div>
 
             <div class="form-group">
               <label for="city">City*</label>
               <input type="text" class="form-control" name="city" id="city" placeholder="Rochester">
-              <label for="street">Postal Code*</label>
+              <label for="postal code">Postal Code*</label>
               <input type="text" class="form-control" name="postal code" id="postal code" placeholder="14627">
+            </div>
+
+            <div class="form-group">
+              <label for = "country">Country</label>
+              <select name="country" class="countries" id="countryId">
+                <option value="">Select Country</option>
+              </select>
             </div>
             
             <div class="form-group">
@@ -100,8 +109,15 @@
             <div class="form-group">
               <label for="city">City</label>
               <input type="text" class="form-control" name="city" id="city" placeholder="Rochester">
-              <label for="street">Postal Code</label>
+              <label for="postal code">Postal Code</label>
               <input type="text" class="form-control" name="postal code" id="postal code" placeholder="14627">
+            </div>
+
+            <div class="form-group">
+              <label for = "country">Country</label>
+              <select name="country" class="countries" id="countryId">
+              <option value="">Select Country</option>
+              </select>
             </div>
             
             <div class="form-group">
@@ -111,12 +127,7 @@
               </select>
             </div>
 
-            <div class="form-group">
-              <label for = "country">Country</label>
-              <select name="country" class="countries" id="countryId">
-                <option value="">Select Country</option>
-              </select>
-            </div>
+
 
             <div class="form-group">
               <label for="tel">Phone</label>
@@ -137,8 +148,15 @@
             <div class="form-group">
               <label for="city">City</label>
               <input type="text" class="form-control" name="city" id="city" placeholder="Rochester">
-              <label for="street">Postal Code</label>
+              <label for="postal code">Postal Code</label>
               <input type="text" class="form-control" name="postal code" id="postal code" placeholder="14627">
+            </div>
+
+            <div class="form-group">
+              <label for = "country">Country</label>
+              <select name="country" class="countries" id="countryId">
+              <option value="">Select Country</option>
+              </select>
             </div>
             
             <div class="form-group">
@@ -149,18 +167,49 @@
             </div>
 
             <div class="form-group">
+              <label for="tel">Phone</label>
+              <input type="tel" class="form-control" name="phone" id="tel" placeholder="(###) ###-####">
+            </div>
+
+            <h2> Foreign Address </h2>
+
+
+            <div class="form-group">
+              <label for="street">Street1</label>
+              <input type="text" class="form-control" name="street" id="street" placeholder="500 Joseph C. Wilson Blvd" autofocus required>
+            </div>
+            <div class="form-group">
+              <label for="street">Street2</label>
+              <input type="text" class="form-control" name="street" id="street" placeholder="">
+            </div>
+
+            <div class="form-group">
+              <label for="city">City</label>
+              <input type="text" class="form-control" name="city" id="city" placeholder="Rochester">
+              <label for="postal code">Postal Code</label>
+              <input type="text" class="form-control" name="postal code" id="postal code" placeholder="14627">
+            </div>
+
+            <div class="form-group">
               <label for = "country">Country</label>
               <select name="country" class="countries" id="countryId">
                 <option value="">Select Country</option>
               </select>
             </div>
+            
+            <div class="form-group">
+              <label for = "state">State</label>
+              <select name="state" class="states" id="stateId">
+                <option value="">Select State</option>
+              </select>
+            </div>
+
 
             <div class="form-group">
               <label for="tel">Phone</label>
               <input type="tel" class="form-control" name="phone" id="tel" placeholder="(###) ###-####">
             </div>
 
-            <h2> Foreign Address </h2>
 
             <h2> Permanent Address</h2>
 
@@ -176,21 +225,22 @@
             <div class="form-group">
               <label for="city">City</label>
               <input type="text" class="form-control" name="city" id="city" placeholder="Rochester">
-              <label for="street">Postal Code</label>
+              <label for="postal code">Postal Code</label>
               <input type="text" class="form-control" name="postal code" id="postal code" placeholder="14627">
-            </div>
-            
-            <div class="form-group">
-              <label for = "state">State</label>
-              <select name="state" class="states" id="stateId">
-                <option value="">Select State</option>
-              </select>
             </div>
 
             <div class="form-group">
               <label for = "country">Country</label>
               <select name="country" class="countries" id="countryId">
                 <option value="">Select Country</option>
+              </select>
+            </div>
+
+            
+            <div class="form-group">
+              <label for = "state">State</label>
+              <select name="state" class="states" id="stateId">
+                <option value="">Select State</option>
               </select>
             </div>
 
@@ -207,15 +257,31 @@
             <h2>Emergency Contact Information</h2>
 
             <div class="form-group">
-              <label for="city">First Name*</label>
+              <label for="first name">First Name*</label>
               <input type="text" class="form-control" name="first name" id="first name" placeholder="Jane">
-              <label for="street">Last Name*</label>
+              <label for="last name">Last Name*</label>
               <input type="text" class="form-control" name="last name" id="last name" placeholder="Doe">
             </div>
 
             <div class="form-group">
-              <label for="city">Relationship*</label>
-              <input type="text" class="form-control" name="relationship" id="relationship" placeholder="">
+              <label for="relationship">Relationship*</label>
+              <select name="relationship" class="relationships" id="relationshipId">
+                <option value="">Select Relationship</option>
+                <option value="foreign mission staff">Foreign Mission Staff</option>
+                <option value="son or daughter">Son or Daughter</option>
+                <option value="spouse">Spouse</option>
+                <option value="relative">Relative</option>
+                <option value="parent">Parent</option>
+                <option value="significant other">Significant Other</option>
+                <option value="grandparent">Grandparent</option>
+                <option value="major superior">Major Superior</option>
+                <option value="brother or sister">Brother or Sister</option>
+                <option value="legal Guardian">Legal Guardian</option>
+                <option value="friend">Friend</option>
+              </select>
+            </div>
+
+            <div class="form-group">
               <label for="email">Email*</label>
               <input type="email" class="form-control" name="email" id="email" placeholder="jane@u.rochester.edu">
             </div>
@@ -230,15 +296,28 @@
               <h3> Parent/Guardian/Family Member </h3>
 
             <div class="form-group">
-              <label for="city">First Name*</label>
+              <label for="first name">First Name*</label>
               <input type="text" class="form-control" name="first name" id="first name" placeholder="Jane">
-              <label for="street">Last Name*</label>
+              <label for="last name">Last Name*</label>
               <input type="text" class="form-control" name="last name" id="last name" placeholder="Doe">
             </div>
 
             <div class="form-group">
-              <label for="city">Relationship*</label>
-              <input type="text" class="form-control" name="relationship" id="relationship" placeholder="">
+              <label for="relationship">Relationship*</label>
+              <select name="relationship" class="relationships" id="relationshipId">
+                <option value="">Select Relationship</option>
+                <option value="son or daughter">Son or Daughter</option>
+                <option value="spouse">Spouse</option>
+                <option value="relative">Relative</option>
+                <option value="parent">Parent</option>
+                <option value="significant other">Significant Other</option>
+                <option value="grandparent">Grandparent</option>
+                <option value="brother or sister">Brother or Sister</option>
+                <option value="legal Guardian">Legal Guardian</option>
+              </select>
+            </div>
+
+            <div class="form-group">
               <label for="email">Email*</label>
               <input type="email" class="form-control" name="email" id="email" placeholder="jane@u.rochester.edu">
             </div>
@@ -251,15 +330,31 @@
             <h3> Local Contact </h3>
 
             <div class="form-group">
-              <label for="city">First Name*</label>
+              <label for="first name">First Name*</label>
               <input type="text" class="form-control" name="first name" id="first name" placeholder="Jane">
-              <label for="street">Last Name*</label>
+              <label for="last name">Last Name*</label>
               <input type="text" class="form-control" name="last name" id="last name" placeholder="Doe">
             </div>
 
             <div class="form-group">
-              <label for="city">Relationship*</label>
-              <input type="text" class="form-control" name="relationship" id="relationship" placeholder="">
+              <label for="relationship">Relationship*</label>
+              <select name="relationship" class="relationships" id="relationshipId">
+                <option value="">Select Relationship</option>
+                <option value="foreign mission staff">Foreign Mission Staff</option>
+                <option value="son or daughter">Son or Daughter</option>
+                <option value="spouse">Spouse</option>
+                <option value="relative">Relative</option>
+                <option value="parent">Parent</option>
+                <option value="significant other">Significant Other</option>
+                <option value="grandparent">Grandparent</option>
+                <option value="major superior">Major Superior</option>
+                <option value="brother or sister">Brother or Sister</option>
+                <option value="legal Guardian">Legal Guardian</option>
+                <option value="friend">Friend</option>
+              </select>
+            </div>
+
+              <div class="form-group">
               <label for="email">Email*</label>
               <input type="email" class="form-control" name="email" id="email" placeholder="jane@u.rochester.edu">
             </div>
