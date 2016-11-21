9c571e0ed323cf48a96bbe04df6d25985cffa6da
Kyle Ryan <kyleryan@Kyles-MacBook-Air.local>
2016-11-20 23:52:14 -0500

    Search Update

--- pages/search.php
+++ pages/search.php
@@ -4,36 +4,48 @@
       <h1>Search</h1>
     </div>
   </div>
-  <form>
-    <div class="form-group">
-      <label for="formGroupExampleInput">Search</label>
-      <input type="text" class="form-control" id="form" placeholder="CSC 219">
-    </div>
-    <input type="button" id="searchButton" onclick="search()" value="Submit"/>  
-  </form> 
-  <div class="container">
-    <div id="results" class="row">
-      <div class="col-xs-12 col-sm-6">
-        Title: CSC 173 Computational and Formal Systems
-      </div>
-    </div>
-  </div> 
-</div> 
+<form>
+        <div class="form-group">
+          <label for="formGroupExampleInput">Search</label>
+          <input type="text" class="form-control" id="form" placeholder="PSY">
+        </div>
+      </form> 
+      <input type="button" id="searchButton" onclick="search()" value="Submit"/>  
+      <div class="container">
+        <div id="results" class="row">
+        </div>
+      </div> 
+      <footer> 
+        <div class="col-md-5">
+          <p> &copy; Meliorats, CSC 210</p>
+        </div>
+      </footer>
+    </div> 
+
 <script type="text/javascript">
-  function search() { 
-    var queryString = document.getElementById("form").value;
-    console.log(document.getElementById("form").value);
-    $.ajax({
-      type: "GET",
-      dataType: 'json',
-      url: "http://www.skedgeur.com/api/courses/?q=" + queryString,
-      async: false,
-      contentType: "application/json; charset=utf-8",
-      success: function (msg) {
-        console.log(msg); // Modify the DOM with contents of each JSON element of each class                
-      }
-    });   
-  }
+function search() { 
+	var node = document.getElementById("results");
+	while (node.hasChildNodes()) {
+    node.removeChild(node.lastChild);
+}
+	var queryString = document.getElementById("form").value;
+	var string = document.getElementById("form").value;
+
+	for (var i = 0; i < 20; i++) {
+		$.ajax({
+		  url: "apirequest.php",
+		  data: {"string": encodeURIComponent(string), "index": i},
+		  type: "GET",
+		  context: document.body,
+		  success: function( result ) {
+		  	var node = document.createElement("p");
+		    var textnode = document.createTextNode(result);
+		    node.appendChild(textnode);
+		    document.getElementById("results").appendChild(node);
+	      }
+  	})
+	}
+}
 </script>
 <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.1.1.min.js"></script>
        
